8f2e57cd68045804d3b290353ede50724e5d8621
Anoj Perera <anojmperera@gmail.com>
2022-12-21 08:37:19 +0000

    LSP key bindings updated

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -27,25 +27,23 @@ lsp.set_preferences({
 	suggest_lsp_servers = false,
 	sign_icons = signs})
 
-lsp.on_attach(function(client, bufnr)
-    local opts = { noremap=true, silent=true }
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  lsp.on_attach(function(client, bufnr)
+    local opts = {buffer = bufnr, remap = false}
 
     -- Mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
-     vim.keymap.set('n', '<space>gD', 'vim.lsp.buf.declaration()<CR>', opts)
-     vim.keymap.set('n', '<space>gd', 'vim.lsp.buf.definition()<CR>', opts)
-     vim.keymap.set('n', '<space>K', 'vim.lsp.buf.hover()<CR>', opts)
-     vim.keymap.set('n', '<space>gi', 'vim.lsp.buf.implementation()<CR>', opts)
-     vim.keymap.set('n', '<space>wa', 'vim.lsp.buf.add_workspace_folder()<CR>', opts)
-     vim.keymap.set('n', '<space>wr', 'vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-     vim.keymap.set('n', '<space>wl', 'print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-     vim.keymap.set('n', '<space>D', 'vim.lsp.buf.type_definition()<CR>', opts)
-     vim.keymap.set('n', '<space>rn', 'vim.lsp.buf.rename()<CR>', opts)
-     vim.keymap.set('n', '<space>ca', 'vim.lsp.buf.code_action()<CR>', opts)
-     vim.keymap.set('n', '<space>gr', 'vim.lsp.buf.references()<CR>', opts)
-     vim.keymap.set('n', '<space>bf', 'vim.lsp.buf.formatting()<CR>', opts)
+    vim.keymap.set('n', '<space>gD', function() vim.lsp.buf.declaration() end, opts)
+    vim.keymap.set('n', '<space>gd', function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set('n', '<space>K', function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set('n', '<space>gi', function() vim.lsp.buf.implementation() end, opts)
+    vim.keymap.set('n', '<space>wa', function() vim.lsp.buf.add_workspace_folder() end, opts)
+    vim.keymap.set('n', '<space>wr', function() vim.lsp.buf.remove_workspace_folder() end, opts)
+    vim.keymap.set('n', '<space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
+    vim.keymap.set('n', '<space>D', function() vim.lsp.buf.type_definition() end, opts)
+    vim.keymap.set('n', '<space>rn', function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set('n', '<space>ca', function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set('n', '<space>gr', function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set('n', '<space>bf', function() vim.lsp.buf.formatting() end, opts)
 
     -- Auto format on save
     if client.server_capabilities.documentFormattingProvider then
