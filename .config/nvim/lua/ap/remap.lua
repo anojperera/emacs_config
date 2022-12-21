f5e9bf1bbed5f706cc7d1692dc8b7dbaf4b7d669
Anoj Perera <anojmperera@gmail.com>
2022-12-21 03:25:39 +0000

    Remap file updated

--- .config/nvim/lua/ap/remap.lua
+++ .config/nvim/lua/ap/remap.lua
@@ -1,2 +1,16 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- Remapped keys for navigating windows
+vim.keymap.set("n", "<leader>h", vim.cmd.h)
+vim.keymap.set("n", "<leader>j", vim.cmd.j)
+vim.keymap.set("n", "<leader>k", vim.cmd.k)
+vim.keymap.set("n", "<leader>l", vim.cmd.l)
+vim.keymap.set("n", "<leader>v", vim.cmd.v)
+vim.keymap.set("n", "<leader>s", vim.cmd.s)
+vim.keymap.set("n", "<leader>q", vim.cmd.q)
+vim.keymap.set("n", "<leader>ps :Rg<SPACE>
+vim.keymap.set("n", "<leader>1", vim.only)
+-- vim.keymap.set("n", "<leader>b", ls<CR>:b<Space>
+-- vim.keymap.set("n", "<silent> <Leader>+ :vertical resize +5<CR>
+-- vim.keymap.set("n", "<silent> <Leader>-5 :vertical resize -5<CR>
+
