a2f5ff0c085192cdf59d34cdede146f7b50a72cd
Anoj Perera <anoj.perera@autodesk.com>
2023-10-17 09:47:30 +0100

    Removed old version lock

--- .config/nvim/lua/ap/packer.lua
+++ .config/nvim/lua/ap/packer.lua
@@ -9,7 +9,7 @@ return require('packer').startup(function(use)
 
   -- Telescope for navigation
   use({
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    'nvim-telescope/telescope.nvim',
     -- or                            , branch = '0.1.x',
     requires = { { 'nvim-lua/plenary.nvim' } }
   })
