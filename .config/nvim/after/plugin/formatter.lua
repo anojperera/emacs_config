e31732d68f6e3859da93af924d3bb8559919424d
Anoj Perera <anojmperera@gmail.com>
2023-12-04 01:15:24 +0000

    conform formatter added

---
+++ .config/nvim/after/plugin/formatter.lua
@@ -0,0 +1,22 @@
+require("conform").setup({
+  formatters_by_ft = {
+    lua = { "stylua" },
+    -- Conform will run multiple formatters sequentially
+    python = { "black" },
+    -- Use a sub-list to run only the first available formatter
+    css = { "prettier"  },
+    html = { "prettier"  },
+    json = { "prettier"  },
+    yaml = { "prettier"  },
+    markdown = { "prettier"  },
+    javascript = { "prettier"  },
+    javascriptreact = { "prettier"  },
+    typescript = { "prettier"  },
+    typescriptreact = { "prettier"  },
+  },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+})
