5b42fe07eceb28a52f0928ccbad4aa8a87f59cea
Anoj Perera <anojmperera@gmail.com>
2024-03-16 17:31:16 +0000

    Removign white space

--- .config/nvim/after/plugin/formatter.lua
+++ .config/nvim/after/plugin/formatter.lua
@@ -13,6 +13,7 @@ require("conform").setup({
     javascriptreact = { "prettier" },
     typescript = { "prettier" },
     typescriptreact = { "prettier" },
+    ["_"] = { "trim_whitespace" },
   },
   format_on_save = {
     -- These options will be passed to conform.format()
