346e59d9cb6c8dc94db05a6ad76a96cb3c7a6547
Anoj Perera <anojmperera@gmail.com>
2024-03-16 17:27:22 +0000

    Removed any format

--- .config/nvim/after/plugin/formatter.lua
+++ .config/nvim/after/plugin/formatter.lua
@@ -13,13 +13,6 @@ require("conform").setup({
     javascriptreact = { "prettier" },
     typescript = { "prettier" },
     typescriptreact = { "prettier" },
-    -- Use the special "*" filetype for defining formatter configurations on
-    -- any filetype
-    ["*"] = {
-      -- "formatter.filetypes.any" defines default configurations for any
-      -- filetype
-      require("formatter.filetypes.any").remove_trailing_whitespace
-    }
   },
   format_on_save = {
     -- These options will be passed to conform.format()
