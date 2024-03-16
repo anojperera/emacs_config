650e42ee6f6406dc8c6816f985562b3892ee75a4
Anoj Perera <anojmperera@gmail.com>
2024-03-16 17:26:03 +0000

    Removing trailing space on all files

--- .config/nvim/after/plugin/formatter.lua
+++ .config/nvim/after/plugin/formatter.lua
@@ -4,15 +4,22 @@ require("conform").setup({
     -- Conform will run multiple formatters sequentially
     python = { "black" },
     -- Use a sub-list to run only the first available formatter
-    css = { "prettier"  },
-    html = { "prettier"  },
-    json = { "prettier"  },
-    yaml = { "prettier"  },
-    markdown = { "prettier"  },
-    javascript = { "prettier"  },
-    javascriptreact = { "prettier"  },
-    typescript = { "prettier"  },
-    typescriptreact = { "prettier"  },
+    css = { "prettier" },
+    html = { "prettier" },
+    json = { "prettier" },
+    yaml = { "prettier" },
+    markdown = { "prettier" },
+    javascript = { "prettier" },
+    javascriptreact = { "prettier" },
+    typescript = { "prettier" },
+    typescriptreact = { "prettier" },
+    -- Use the special "*" filetype for defining formatter configurations on
+    -- any filetype
+    ["*"] = {
+      -- "formatter.filetypes.any" defines default configurations for any
+      -- filetype
+      require("formatter.filetypes.any").remove_trailing_whitespace
+    }
   },
   format_on_save = {
     -- These options will be passed to conform.format()
