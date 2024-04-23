a4fe384bb97c3f09f1ca3058a57713302d0a2ef3
Anoj Perera <anojmperera@gmail.com>
2024-04-23 12:43:15 +0100

    Updated config for trouble

---
+++ .config/nvim/after/plugin/trouble.lua
@@ -0,0 +1,7 @@
+local builtin = require("trouble")
+vim.keymap.set("n", "<leader>xx", function() builtin.toggle() end)
+vim.keymap.set("n", "<leader>xw", function() builtin.toggle("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>xd", function() builtin.toggle("document_diagnostics") end)
+vim.keymap.set("n", "<leader>xq", function() builtin.toggle("quickfix") end)
+vim.keymap.set("n", "<leader>xl", function() builtin.toggle("loclist") end)
+vim.keymap.set("n", "gR", function() builtin.toggle("lsp_references") end)
