fe35a22b2217f3e153b7e1d4d29fd8a47c2a0122
Anoj Perera <anoj.perera@dxc.com>
2025-10-27 12:00:50 +0000

    Updated config

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,28 @@
+return {
+	"stevearc/conform.nvim",
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Conform will run multiple formatters sequentially
+				python = { "autopep8", "black" },
+				-- Use a sub-list to run only the first available formatter
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				javascript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescript = { "prettier" },
+				typescriptreact = { "prettier" },
+				["_"] = { "trim_whitespace" },
+			},
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 2000,
+				lsp_fallback = true,
+			},
+		})
+	end,
+}
